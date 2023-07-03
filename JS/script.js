7c4c8f20d34640976337e2ef4cc06ee22057b0d0
mathinicarole <raquelcarole049@gmail.com>
2023-07-03 07:52:07 +0300

    change

--- JS/script.js
+++ JS/script.js
@@ -56,3 +56,42 @@ ScrollReveal().reveal('.home-content p, .about-content',{ origin:'right'}) ;
 //   backDelay:1000,
 //   loop: true
 // });
+
+
+// header sticky
+window.addEventListener("scroll", function(){
+  var header= document.querySelector("header");
+  header.classList.toggle("sticky" , window.scrollY >0);
+})
+// common reveal options to create reveal animations
+ScrollReveal({
+  // reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
+// traget elements, and specify options to create reveal animations
+ScrollReveal().reveal(".main-title, .second-title, .info-title", {
+  delay: 500,
+  origin: "left",
+});
+ScrollReveal().reveal(".sec-01 .image,.sec-04 .image, .info", {
+  delay: 600,
+  origin: "bottom",
+});
+ScrollReveal().reveal(".text-box", { delay: 700, origin: "right" });
+ScrollReveal().reveal(".media-icons i", {
+  delay: 500,
+  origin: "bottom",
+  interval: 200,
+});
+ScrollReveal().reveal(".sec-02 .image, .sec-03 .image", {
+  delay: 500,
+  origin: "top",
+});
+ScrollReveal().reveal(".media-info li", {
+  delay: 500,
+  origin: "left",
+  interval: 200,
+});
